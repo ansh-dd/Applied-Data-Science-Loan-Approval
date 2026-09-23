4e8ceed98ffd78aae6849dbcdb13b43e5cfc1fe2
Ansh Thakur <ansh24comp@student.mes.ac.in>
2026-09-23 18:48:10 +0530

    Fix final Ruff import order

--- exp6_api/test_app.py
+++ exp6_api/test_app.py
@@ -1,8 +1,8 @@
-from app import app
 from fastapi.testclient import TestClient
 
-client = TestClient(app)
+from app import app
 
+client = TestClient(app)
 
 def test_root():
     response = client.get("/")
